de48706ab04367c33bbde9030b11795e5d5c0480
Huang, Po-Hsiang <87964932+Ryan-0911@users.noreply.github.com>
2023-06-19 15:52:22 +0800

    midterm MVC

---
+++ ChilLaxBackEnd/Controllers/PurchaseController.cs
@@ -0,0 +1,153 @@
+﻿using ChilLaxBackEnd.Models.ViewModels;
+using ChilLaxBackEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace ChilLaxBackEnd.Controllers
+{
+	public class PurchaseController : Controller
+	{
+		// GET: PurchaseController
+		[HttpGet]
+		public ActionResult Index(int? nowpage, int? _pageCount)
+		{
+			ChilLaxEntities db = new ChilLaxEntities();
+
+			if (nowpage == null)
+			{
+				nowpage = 1;
+			}
+			int? pageCount = _pageCount;
+
+			if (_pageCount == null)
+			{
+				int dataCount = db.ProductOrder.Count();
+				pageCount = dataCount / 10;
+				if (dataCount % 10 != 0) pageCount += 1;
+			}
+
+
+			List<ProductOrderDetail> productOrderDetails = db
+				.ProductOrder
+				.OrderByDescending(p => p.order_date)
+				.Skip(10 * ((int)nowpage) - 1)
+				.Take(10)
+				.Join(db.OrderDetail,
+					po => po.order_id,
+					od => od.order_id,
+					(po, od) => new ProductOrderDetail
+					{
+						ProductOrder = po,
+						OrderDetail = od
+					}).ToList();
+
+			productOrderDetails.FirstOrDefault().pageCount = pageCount;
+			productOrderDetails.FirstOrDefault().nowpage = nowpage;
+
+			return View(productOrderDetails);
+		}
+		//public ActionResult Index(int nowpage, int pageCount)
+		//{
+
+		//    ChilLaxEntities db = new ChilLaxEntities();
+
+		//    List<ProductOrderDetail> productOrderDetails = db
+		//        .ProductOrder
+		//        .OrderByDescending(p => p.order_date)
+		//        .Skip(10 * ((int)nowpage) - 1)
+		//        .Take(10)
+		//        .Join(db.OrderDetail,
+		//            po => po.order_id,
+		//            od => od.order_id,
+		//            (po, od) => new ProductOrderDetail
+		//            {
+		//                ProductOrder = po,
+		//                OrderDetail = od
+		//            }).ToList();
+
+		//    productOrderDetails.FirstOrDefault().nowpage = nowpage;
+		//    productOrderDetails.FirstOrDefault().pageCount = pageCount;
+
+		//    return View(productOrderDetails);
+		//}
+
+		// GET: Purchas/Details/5
+		public ActionResult Details(int id)
+		{
+			if (id != null)
+			{
+				ChilLaxEntities db = new ChilLaxEntities();
+				ProductOrder prod = db.ProductOrder.FirstOrDefault(p => p.order_id == id);
+				OrderDetail detail = db.OrderDetail.FirstOrDefault(p => p.order_id == id);
+				if (prod != null || detail != null)
+				{
+					db.ProductOrder.Remove(prod);
+					db.OrderDetail.Remove(detail);
+					db.SaveChanges();
+				}
+			}
+			return RedirectToAction("Index");
+		}
+
+		// GET: Purchas/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id != null)
+			{
+				ChilLaxEntities db = new ChilLaxEntities();
+				List<ProductOrderDetail> productOrderDetails = db.ProductOrder
+					.Join(db.OrderDetail,
+						po => po.order_id,
+						od => od.order_id,
+						(po, od) => new ProductOrderDetail
+						{
+							ProductOrder = po,
+							OrderDetail = od
+						})
+					.Where(po => po.ProductOrder.order_id == id)
+					.ToList();
+
+				List<SelectListItem> mySelectItemList = new List<SelectListItem>(){
+					new SelectListItem{Text="未出貨", Value="未出貨", Selected=productOrderDetails.First().ProductOrder.order_state=="未出貨"},
+					new SelectListItem{Text="已出貨", Value="已出貨", Selected=productOrderDetails.First().ProductOrder.order_state=="已出貨"},
+					new SelectListItem{Text="已完成", Value="已完成", Selected=productOrderDetails.First().ProductOrder.order_state=="已完成"}
+				};
+
+				productOrderDetails.First().orderStateSelectedList = mySelectItemList;
+
+				return View(productOrderDetails);
+			}
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public ActionResult Edit(ProductOrderDetail po)
+		{
+			try
+			{
+				ChilLaxEntities db = new ChilLaxEntities();
+				ProductOrder prod = db.ProductOrder.FirstOrDefault(p => p.order_id == po.ProductOrder.order_id);
+				if (prod != null)
+				{
+					prod.order_payment = po.ProductOrder.order_payment;
+					prod.order_state = po.ProductOrder.order_state;
+					prod.order_delivery = po.ProductOrder.order_delivery;
+					prod.order_address = po.ProductOrder.order_address;
+					prod.order_note = po.ProductOrder.order_note;
+					db.SaveChanges();
+				}
+				//TODO: Add update logic here
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View(po);
+			}
+		}
+	}
+}
